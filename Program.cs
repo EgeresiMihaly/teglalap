7e0dd3929c7a8cb01293ab5c3eacaa7730aefdf1
Linuxfun51 <91023363+EgeresiMihaly@users.noreply.github.com>
2023-06-28 17:02:36 +0200

    elso c# consol app

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Kérem a téglalap magasságát:");
+int height = int.Parse(Console.ReadLine());
+
+Console.Write("Kérem a téglalap szélességét:");
+int width = int.Parse(Console.ReadLine());
+
+int perimeter = 2 * (height+width);
+int area = height*width;
+
+Console.WriteLine("A téglalap kerülete :" +perimeter);
+Console.WriteLine("A téglalap területe : " +area);
